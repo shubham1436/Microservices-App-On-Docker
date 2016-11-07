4568749680ac7f19dfcb982e51fda09d63caed65
Ubuntu <shubham1436@DockerSwarmWorker1.4pl1lqrum12ufa5kfto22gzr3e.ix.internal.cloudapp.net>
2016-11-07 10:23:02 +0000

    Docker Image Build Delegated to Docker Compose

--- build-all.sh
+++ build-all.sh
@@ -4,27 +4,29 @@ set -e
 
 cd util;                                              ./gradlew clean build publishToMavenLocal; cd -
 
-cd microservices/core/product-service;                ./gradlew clean build buildDockerImage; cd -
-cd microservices/core/recommendation-service;         ./gradlew clean build buildDockerImage; cd -
-cd microservices/core/review-service;                 ./gradlew clean build buildDockerImage; cd -
-cd microservices/composite/product-composite-service; ./gradlew clean build buildDockerImage; cd -
+cd microservices/core/product-service;                ./gradlew clean build; cd -
+cd microservices/core/recommendation-service;         ./gradlew clean build; cd -
+cd microservices/core/review-service;                 ./gradlew clean build; cd -
+cd microservices/composite/product-composite-service; ./gradlew clean build; cd -
 
-cd microservices/support/auth-server;                 ./gradlew clean build buildDockerImage; cd -
-cd microservices/support/config-server;               ./gradlew clean build buildDockerImage; cd -
-cd microservices/support/discovery-server;            ./gradlew clean build buildDockerImage; cd -
-cd microservices/support/edge-server;                 ./gradlew clean build buildDockerImage; cd -
-cd microservices/support/monitor-dashboard;           ./gradlew clean build buildDockerImage; cd -
-cd microservices/support/turbine;                     ./gradlew clean build buildDockerImage; cd -
+cd microservices/support/auth-server;                 ./gradlew clean build; cd -
+cd microservices/support/config-server;               ./gradlew clean build; cd -
+cd microservices/support/discovery-server;            ./gradlew clean build; cd -
+cd microservices/support/edge-server;                 ./gradlew clean build; cd -
+cd microservices/support/monitor-dashboard;           ./gradlew clean build; cd -
+cd microservices/support/turbine;                     ./gradlew clean build; cd -
 
 find . -name *SNAPSHOT.jar -exec du -h {} \;
 
-docker push $1/ms-blog-discovery-server;
-docker push $1/ms-blog-monitor-dashboard;
-docker push $1/ms-blog-edge-server;
-docker push $1/ms-blog-config-server;
-docker push $1/ms-blog-auth-server;
-docker push $1/ms-blog-product-composite-service;
-docker push $1/ms-blog-review-service;
-docker push $1/ms-blog-recommendation-service;
-docker push $1/ms-blog-product-service;
-docker push $1/ms-blog-turbine;
+docker-compose -f docker-compose-dev.yml build
+
+//docker push $1/ms-blog-discovery-server;
+//docker push $1/ms-blog-monitor-dashboard;
+//docker push $1/ms-blog-edge-server;
+//docker push $1/ms-blog-config-server;
+//docker push $1/ms-blog-auth-server;
+//docker push $1/ms-blog-product-composite-service;
+//docker push $1/ms-blog-review-service;
+//docker push $1/ms-blog-recommendation-service;
+//docker push $1/ms-blog-product-service;
+//docker push $1/ms-blog-turbine;
